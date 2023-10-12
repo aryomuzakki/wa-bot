9a2457a7294d821920bb206561ff07952e236e17
Muhammad Aryo Muzakki <aryo.muzakki@gmail.com>
2023-10-12 18:03:31 +0700

    add youtube list

--- chat.js
+++ chat.js
@@ -1,4 +1,9 @@
-const handleIncomingMessage = (theClient, message) => {
+const { youtube } = require("scrape-youtube");
+const ytsr = require("ytsr");
+
+const handleIncomingMessage = async (theClient, message) => {
+
+  const messageBody = message.body.toLowerCase();
 
   // log
   console.log("New Incoming Message: ");
@@ -16,30 +21,8 @@ const handleIncomingMessage = (theClient, message) => {
     return;
   }
 
-  // all chat
-
-  // hello message
-  if (message.body.toLowerCase() === "hai zak") {
-    message.reply("Halo, zak disini\n\nAda yang bisa saya bantu?");
-    // theClient.sendMessage(message.from, "Halo, Ryo disini\n\nAda yang bisa saya bantu?");
-  }
-
-  // sticker maker
-  if (message.body.toLowerCase() === "hai zak, tolong buat jadi sticker") {
-    if (message.hasMedia) {
-      (async () => {
-        const media = await message.downloadMedia();
-        theClient.sendMessage(message.from, media, { sendMediaAsSticker: true, stickerAuthor: "aryo", stickerName: "reply", stickerCategories: ["bot", "random"] });
-      })();
-    } else {
-      message.reply("Sertakan Fotonya");
-    }
-  }
-
-  // personal chat
-  if (!message.isGroup) {
-
-  }
+  // incoming message contact
+  const contact = await message.getContact();
 
   // group
   if (message.isGroup) {
@@ -47,6 +30,81 @@ const handleIncomingMessage = (theClient, message) => {
     // mention all participant in a group
 
   }
+
+  // personal chat
+  if (!message.isGroup) {
+
+  }
+
+  // all chat
+
+  // hello message
+  if (messageBody === "hai zak") {
+
+    message.reply(`Halo, ${contact.pushname}\n\nSilahkan pilih menu dibawah:\n  - Sticker WA (Buat sticker wa dari gambar)\n  - Youtube (cari/download youtube)\n  - Katalog (list harga produk)\n  - Cara order (info cara pesan lewat wa bot)\n  - Status order (status transaksi order)`);
+    // theClient.sendMessage(message.from, "Halo,\n\nAda yang bisa saya bantu?");
+  }
+
+  // sticker maker
+  if (messageBody === "buat sticker wa") {
+    if (message.hasMedia) {
+      (async () => {
+        const media = await message.downloadMedia();
+        theClient.sendMessage(message.from, media, { sendMediaAsSticker: true, stickerAuthor: "aryo", stickerName: "reply", stickerCategories: ["bot", "random"] });
+      })();
+    } else {
+      message.reply("Maaf, kamu harus mengirimkan bersamaan dengan fotonya");
+    }
+  }
+
+  // Youtube
+  if (messageBody.startsWith("list youtube")) {
+    const searchQuery = messageBody.replace("list youtube", "").split("sebanyak")[0].trim();
+
+    if (searchQuery.length < 2) {
+      message.reply("Maaf~ Teks pencarian yang harus diberikan minimal 2 huruf");
+      return;
+    }
+
+    const listCount = parseInt(messageBody.split("sebanyak")[1].trim().split(" ")[0] ?? 5);
+
+    const filter = (await ytsr.getFilters(searchQuery)).get("Type").get("Video");
+
+    const searchResults = await ytsr(filter.url, {
+      gl: "ID",
+      hl: "id",
+      safeSearch: true,
+      limit: listCount + (listCount % 5),
+    });
+
+    const videoResults = searchResults.items.filter(item => item.type == "video").slice(0, listCount);
+
+    let replyString = `Berikut hasil pencarian youtube "${searchQuery}"\n`;
+
+    const resultItems = videoResults.map((item, idx) => {
+      const duration = (item.duration || "").split(".").reverse();
+      const jam = (duration?.[2]) ? duration[2] + " jam " : "";
+
+      const formatter = new Intl.NumberFormat('id-ID');
+
+      const resultItem = {
+        judul: item.title,
+        link: item.url,
+        channel: item.author.name + (item.author?.ownerBadges ? ` (${item.author.ownerBadges})` : ""),
+        durasi: jam + parseInt(duration[1]).toString() + " menit " + duration[0] + " detik",
+        diuploadPada: item.uploadedAt,
+        totalDilihat: formatter.format(item.views),
+      }
+
+      replyString += `\n${++idx}. Judul: ${resultItem.judul}\nLink: ${resultItem.link}\nChannel: ${resultItem.channel}\nDurasi: ${resultItem.durasi}\nDi Upload Pada: ${resultItem.diuploadPada}\nJumlah Dilihat: ${resultItem.totalDilihat}\n\n`;
+
+      return resultItem;
+    })
+
+    message.reply(replyString)
+
+  }
+
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { youtube } = require("scrape-youtube");
 const { startClient } = require("./lib/waweb");
+const ytsr = require("ytsr");
 
 const fastify = require("fastify")({ logger: true });
 
@@ -53,14 +54,63 @@ fastify.get("/getqr", (request, reply) => {
 
 fastify.get("/listyt/:search?", async (request, reply) => {
 
-  const searchQuery = request.params?.search || request.query?.search || request.body?.search;
-  const type = request.query?.type || request.body?.type;
+  // const searchQuery = request.params?.search || request.query?.search || request.body?.search;
+  // const type = request.query?.type || request.body?.type;
 
-  const results = await youtube.search(searchQuery, { type });
+  // const results = await youtube.search(searchQuery, { type });
 
-  console.log("results: ", results)
-  console.log("results.videos: ", results.videos)
-  return reply.send({ success: true, data: results.videos });
+  // console.log("results: ", results)
+  // console.log("results.videos: ", results.videos)
+  // return reply.send({ success: true, data: results.videos });
+
+  const messageBody = request.params?.messageBody || request.query?.messageBody || request.body?.messageBody || "";
+
+  if (!messageBody) {
+    return reply.code(400).send({ success: false, message: "messageBody is required" });
+  }
+
+  const searchQuery = messageBody.replace("list youtube", "").split("sebanyak")[0].trim();
+
+  if (searchQuery.length < 2) {
+    return reply.code(400).send({ success: false, message: "Maaf~ Teks pencarian yang harus diberikan minimal 2 huruf" });
+  }
+
+  const listCount = parseInt(messageBody.split("sebanyak")[1].trim().split(" ")[0] ?? 5);
+
+  const filter = (await ytsr.getFilters(searchQuery)).get("Type").get("Video");
+
+  const searchResults = await ytsr(filter.url, {
+    gl: "ID",
+    hl: "id",
+    safeSearch: true,
+    limit: listCount + (listCount % 5),
+  });
+
+  const videoResults = searchResults.items.filter(item => item.type == "video").slice(0, listCount);
+
+  let replyString = `Berikut hasil pencarian youtube "${searchQuery}"\n`;
+
+  const resultItems = videoResults.map((item, idx) => {
+    const duration = (item.duration || "").split(".").reverse();
+    const jam = (duration?.[2]) ? duration[2] + " jam " : "";
+
+    const formatter = new Intl.NumberFormat('id-ID');
+
+    const resultItem = {
+      judul: item.title,
+      link: item.url,
+      channel: item.author.name + (item.author?.ownerBadges ? ` (${item.author.ownerBadges})` : ""),
+      durasi: jam + parseInt(duration[1]).toString() + " menit " + duration[0] + " detik",
+      diuploadPada: item.uploadedAt,
+      totalDilihat: formatter.format(item.views),
+    }
+
+    replyString += `\n${++idx}. Judul: ${resultItem.judul}\nLink: ${resultItem.link}\nChannel: ${resultItem.channel}\nDurasi: ${resultItem.durasi}\nDi Upload Pada: ${resultItem.diuploadPada}\nJumlah Dilihat: ${resultItem.totalDilihat}\n\n`;
+
+    return resultItem;
+  })
+
+  return reply.send({ success: true, data: resultItems, replyString });
 })
 
 const startServer = (port) => {
